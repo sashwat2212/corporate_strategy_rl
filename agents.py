7b51621229bacebede186bd1aed5f8a412457627
sashwat2212 <sinha.sashwat02@gmail.com>
2025-02-13 20:48:45 +0530

    added comments

--- agents.py
+++ agents.py
@@ -1,39 +1,7 @@
-# import numpy as np
-# import random
-
-# class QLearningAgent:
-#     def __init__(self, state_size, action_size, learning_rate=0.1, discount_factor=0.95, exploration_rate=1.0, min_exploration=0.01, decay_rate=0.995):
-#         self.state_size = state_size
-#         self.action_size = action_size
-#         self.learning_rate = learning_rate  # Alpha
-#         self.discount_factor = discount_factor  # Gamma
-#         self.exploration_rate = exploration_rate  # Epsilon for exploration
-#         self.min_exploration = min_exploration  # Minimum epsilon
-#         self.decay_rate = decay_rate  # Decay rate for epsilon
-        
-#         # Initialize Q-table with zeros
-#         self.q_table = np.zeros((state_size, action_size))
-    
-#     def choose_action(self, state):
-#         if random.uniform(0, 1) < self.exploration_rate:
-#             return random.randint(0, self.action_size - 1)  # Explore (random action)
-#         else:
-#             return np.argmax(self.q_table[state])  # Exploit (best known action)
-    
-#     def update_q_table(self, state, action, reward, next_state):
-#         best_next_action = np.argmax(self.q_table[next_state])
-#         td_target = reward + self.discount_factor * self.q_table[next_state, best_next_action]
-#         td_error = td_target - self.q_table[state, action]
-#         self.q_table[state, action] += self.learning_rate * td_error
-    
-#     def decay_exploration(self):
-#         self.exploration_rate = max(self.min_exploration, self.exploration_rate * self.decay_rate)
-
-
-
 import numpy as np
 import random
 
+# Define the QLearningAgent class
 class QLearningAgent:
     def __init__(self, capital_bins=20, market_share_bins=20, action_size=4, learning_rate=0.1, discount_factor=0.95, exploration_rate=1.0, min_exploration=0.01, decay_rate=0.995):
         self.capital_bins = capital_bins
@@ -45,12 +13,14 @@ class QLearningAgent:
         self.min_exploration = min_exploration
         self.decay_rate = decay_rate
 
+        # Initialize total spent and initial market share
         self.total_spent = 0
         self.initial_market_share = None
         
         # Initialize Q-table with zeros
         self.q_table = np.zeros((self.capital_bins, self.market_share_bins, self.action_size))
     
+    # Discretize the state space
     def discretize_state(self, state):
         capital, market_share = state
         
@@ -60,6 +30,7 @@ class QLearningAgent:
         
         return capital_bin, market_share_bin
     
+    # Choose an action based on the current state
     def choose_action(self, state):
         capital_bin, market_share_bin = self.discretize_state(state)
         
@@ -68,6 +39,7 @@ class QLearningAgent:
         else:
             return np.argmax(self.q_table[capital_bin, market_share_bin])  # Exploit (best known action)
     
+    # Update the Q-table based on the state, action, reward, and next state
     def update_q_table(self, state, action, reward, next_state):
         capital_bin, market_share_bin = self.discretize_state(state)
         next_capital_bin, next_market_share_bin = self.discretize_state(next_state)
@@ -78,5 +50,6 @@ class QLearningAgent:
         
         self.q_table[capital_bin, market_share_bin, action] += self.learning_rate * td_error
     
+    # Decay the exploration rate
     def decay_exploration(self):
         self.exploration_rate = max(self.min_exploration, self.exploration_rate * self.decay_rate)
